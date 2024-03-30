f4ef1370586a3689c52f744ab0ce072521d9a7ae
lufisaal <lalves.fs@gmail.com>
2024-03-30 19:02:02 +0000

    arrange code

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,6 @@ WORKDIR ${CATALINA_HOME}
 
 # Install necessary packages
 RUN yum -y install java-1.8.0-openjdk openssl
-# INSTALL tomcat
 RUN curl -O https://downloads.apache.org/tomcat/tomcat-8/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz && \
     tar xvfz apache-tomcat-*.tar.gz && \
     mv apache-tomcat-${TOMCAT_VERSION}/* ${CATALINA_HOME}/. && \
@@ -22,10 +21,11 @@ RUN curl -O -L https://tomcat.apache.org/tomcat-8.5-doc/appdev/sample/sample.war
 # Copy SSL certificate and key
 COPY tomcat-cert.pem ${CATALINA_HOME}/conf/
 COPY tomcat-key.pem ${CATALINA_HOME}/conf/
-# converting cert-key to pkcs12
+
+# Converting cert-key to pkcs12
 RUN openssl pkcs12 -export -in ${CATALINA_HOME}/conf/tomcat-cert.pem -inkey ${CATALINA_HOME}/conf/tomcat-key.pem -out ${CATALINA_HOME}/conf/tomcat-keystore.p12 -name tomcat -password pass:testomcat
 
-#Config tomcat 
+# Config tomcat 
 COPY server.xml ${CATALINA_HOME}/conf
 
 EXPOSE 4041
--- script.sh
+++ script.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# # Function to check if a command exists
-# command_exists() {
-#     command -v "$@" > /dev/null 2>&1
-# }
-
-# # Check for Docker
-# if ! command_exists docker; then
-#     echo "Docker is not installed. Attempting to install Docker..."
-#     # Installation command for Docker will vary based on the OS
-#     # For Ubuntu/Debian, uncomment the following line:
-#     # sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-#     # For other OS, modify accordingly or provide instructions
-# fi
-
-# # Check for OpenSSL
-# if ! command_exists openssl; then
-#     echo "OpenSSL is not installed. Attempting to install OpenSSL..."
-#     # Installation command for OpenSSL
-#     # For Ubuntu/Debian, uncomment the following line:
-#     # sudo apt-get install openssl -y
-#     # For other OS, modify accordingly or provide instructions
-# fi
-
 # Generate SSL certificates if they do not exist
 if [ ! -f "tomcat-key.pem" ] || [ ! -f "tomcat-cert.pem" ]; then
     echo "Generating SSL certificates..."
